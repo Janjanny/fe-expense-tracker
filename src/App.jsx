2ae70ab92fb934bd8a11c4d66c8c357df1fa2aed
Janjanny <janjanbeboso0912@gmail.com>
2024-06-02 00:55:46 +0800

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,59 @@ function App() {
         <div className="header">
           <h2>Dashboard</h2>
         </div>
+
+        <div className="bubble">
+          <p className="budget"><span>$</span>10,000</p>
+
+          <p className="currency">PHP</p>
+        </div>
+
+        <div className="expense-section">
+          <div className="header">
+            <p>Expenses</p>
+            <button className="budget-btn">Add Budget</button>
+          </div>
+
+          {/* expenses entry */}
+        <div className="expense-card">
+          <div className="tip-color"></div>
+          <div className="expense-detail">
+            <p className="name">Groceries</p>
+            <p className="description">Lorem, ipsum dolor.</p>
+          </div>
+          <div className="expense-amount">
+            <p className="amount"><span>$</span> 9.99</p>
+          </div>
+        </div>
+
+        <div className="expense-card">
+          <div className="tip-color"></div>
+          <div className="expense-detail">
+            <p className="name">Groceries</p>
+            <p className="description">Lorem, ipsum dolor.</p>
+          </div>
+          <div className="expense-amount">
+            <p className="amount"><span>$</span> 9.99</p>
+          </div>
+        </div>
+
+        <div className="expense-card">
+          <div className="tip-color"></div>
+          <div className="expense-detail">
+            <p className="name">Groceries</p>
+            <p className="description">Lorem, ipsum dolor.</p>
+          </div>
+          <div className="expense-amount">
+            <p className="amount"><span>$</span> 9.99</p>
+          </div>
+        </div>
+        </div>
+
+        <button className="add-btn">
+          +
+        </button>
+
+        
       </div>
     </>
   )
