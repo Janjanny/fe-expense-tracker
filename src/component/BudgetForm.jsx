28d9ca443bed39274633767f53e7ab8dd08c5846
Janjanny <janjanbeboso0912@gmail.com>
2024-06-07 22:59:44 +0800

    add styles

--- src/component/BudgetForm.jsx
+++ src/component/BudgetForm.jsx
@@ -18,12 +18,14 @@ const BudgetForm = ({closeModalBudget, setBudget}) => {
 
    
     return (<>
-        <div className="budget-card">
-            <h2>Add Budget</h2>
-            <div className="currency">PHP</div>
-            <input type="number" className="input-amount" value={amount} onChange={handleInputChange} />
-            <button className="close-btn" onClick={closeModalBudget}>Close</button>
-            <button className="submit-btn" onClick={addBudget}>Submit</button>
+        <div className="budget-card-backdrop">
+            <div className="budget-card">
+                <h2>Add Budget</h2>
+                <div className="currency">PHP</div>
+                <input type="number" className="input-amount" value={amount} onChange={handleInputChange} />
+                <button className="close-btn" onClick={closeModalBudget}>Close</button>
+                <button className="submit-btn" onClick={addBudget}>Submit</button>
+            </div>
         </div>
       </>)
 }
